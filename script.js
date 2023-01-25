4d9adb8763c6356d393a73f55ec41c29fa26a53f
anonymousMornach <nou213090168@noun.edu.ng>
2023-01-25 13:33:21 +0100

    minor modifications

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 const buttons = document.querySelectorAll('button');
-const crashAudio = new Audio("./sounds/crash.mp3");
-const kickBassAudio = new Audio("./sounds/kick-bass.mp3");
-const snareAudio = new Audio("./sounds/snare.mp3");
-const tom1Audio = new Audio("./sounds/tom-1.mp3");
-const tom2Audio = new Audio("./sounds/tom-2.mp3");
-const tom3Audio = new Audio("./sounds/tom-3.mp3");
-const tom4Audio = new Audio("./sounds/tom-4.mp3");
 buttons.forEach((button)=>{
 	button.addEventListener('click', ()=>{
 		handleSound(button.textContent);
@@ -20,24 +13,31 @@ function handleSound(value){
 	switch(value)
 	{
 		case 'w':
+			const tom1Audio = new Audio("./sounds/tom-1.mp3");
 			tom1Audio.play();
 			break;
 		case 'a':
+			const tom2Audio = new Audio("./sounds/tom-2.mp3");
 			tom2Audio.play();
 			break;
 		case 's':
+			const tom3Audio = new Audio("./sounds/tom-3.mp3");
 			tom3Audio.play();
 			break;
 		case 'd':
+			const tom4Audio = new Audio("./sounds/tom-4.mp3");
 			tom4Audio.play();
 			break;
 		case 'j':
+			const snareAudio = new Audio("./sounds/snare.mp3");
 			snareAudio.play();
 			break;
 		case 'k':
+			const crashAudio = new Audio("./sounds/crash.mp3");
 			crashAudio.play();
 			break;
 		case 'l':
+			const kickBassAudio = new Audio("./sounds/kick-bass.mp3");
 			kickBassAudio.play();
 			break;
 	}
